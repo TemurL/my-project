0c69c326e67a3dd2e3ae153f30669e39cbae9318
Temur Lakerbaya <lakerbaya00@mail.ru>
2023-02-17 20:46:11 +0300

    function done

--- firstSub/scripts/script.js
+++ firstSub/scripts/script.js
@@ -1,12 +1,18 @@
 const h1 = document.querySelector('h1');
+const p = document.querySelector('p')
 
 const colorChenge = (elem) => {
+    elem.style.transition = '.3s'
     setInterval(() => {
         elem.style.color = 'red';
         setTimeout(() => {
             elem.style.color = 'black'
-        }, 1000)
-    }, 2000)
+        }, 1000);
+        setTimeout(() => {
+            elem.style.color = 'blue'
+        }, 2000);
+    
+    }, 3000)
 }
 
 colorChenge(h1)
